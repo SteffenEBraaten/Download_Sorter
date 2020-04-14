d70a82a91d50413b1c87c1d47599f5c35676bd2b
SteffenEBraaten <steffen.e.braaten@gmail.com>
2020-04-14 19:39:12 +0200

    Made it so that the system finds the users folder automatically. Refactored some code and added documentation to some of the functions. Removed set up from README.md as it's not necessary.

--- README.md
+++ README.md
@@ -4,9 +4,6 @@ A simple Python program to keep your downloads folder nice and tidy.
 This program will sort your files in diffrent folders based on it's extension type. You are free to add/remove any folder 
 or extensions you may want by manipulating the file_extensions.txt file. Some of the file extension in this file is based
 upon [FileInfos common file types list](https://fileinfo.com/filetypes/common).
-## Set up
-At the current time you will need to set the path to the folder that you would like the program to sort. Simply change the
-variable "folder_to_sort" to your own path.
 ## Running the program
 To run the program simply write the following:
 ```bash
--- download_sorter.py
+++ download_sorter.py
@@ -1,61 +1,69 @@
 import os
-import shutil
+import sys
 
-#TODO
-# Find a way to find the downloads folder to a user without an explicit path
-
-# Change the "folder_to_sort" to the path of the folder you want to sort
-folder_to_sort = '/Users/YOUR_USERNAME/Desktop/to_be_moved'
+folder_to_sort = '{}/Downloads/'.format(os.path.expanduser('~'))
 filepath = 'file_extensions.txt'
 
-exstension = {}
+exstensions = {}
 
 def read_file(file):
+    """
+    Reads the extensions in the file and loads them into the extensions dictionary. 
+    
+    If the file format is not correct an exception will be thrown and the program
+    will be terminate.
+    """
+    
     try:
-        print("Reading from file...")
         with open(file, 'r') as fe:
-            for line in __nonblank_lines(fe):
+            print('Reading from file...')
+            for line in fe:
                 word = line.split()
                 __make_folder(word[0])
-                exstension[word[0]] = []
+                exstensions[word[0]] = []
                 for i in range(1, len(word)):
-                    exstension[word[0]].append(word[i])                    
-    finally:
+                    exstensions[word[0]].append(word[i])
         __make_folder('Other')
         print('Reading from file completed!')
-        fe.close()
+    except IOError as e:
+        print(e)
+        sys.exit()
 
-def sort():
+def sort_files():
+    """
+    Sorting the files in the selected folder based on the extensions which is
+    loaded from the file read in read_file(file). 
+    
+    If the fiel extension matches one of the extentions it will be moved to 
+    the folder that the extension relates to.
+
+    However if the extension does not match any of our extensions it will
+    be placed in the "Other" folder.
+    """
+
     print('Sorting started...')
-    for file_name in os.listdir(folder_to_sort):
-        source = folder_to_sort + '/' + file_name
-        for key in exstension:
-            for value in exstension[key]:
-                if file_name.endswith(value):
-                    source = folder_to_sort + '/' + file_name
-                    destination = folder_to_sort + '/' + key + '/' + file_name
+    for file in os.listdir(folder_to_sort):
+        source = '{}/{}'.format(folder_to_sort, file)
+        for key in exstensions:
+            for value in exstensions[key]:
+                if file.lower().endswith(value):
+                    destination = '{}/{}/{}'.format(folder_to_sort, key, file)
                     __move_file(source, destination)
-        other = folder_to_sort + '/Other/' + file_name
+        other = '{}/Other/{}'.format(folder_to_sort, file)
         __move_file(source, other)
     print('Sorting completed!')
 
-def __nonblank_lines(file):
-    for line in file:
-        line = line.rstrip()
-        if line:
-            yield line
-
-def __make_folder(name):
-    new_folder = folder_to_sort + '/' + name
+def __make_folder(folder_name):
+    new_folder = '{}/{}'.format(folder_to_sort, folder_name)
     if not os.path.exists(new_folder):
         os.mkdir(new_folder)
 
 def __move_file(source, destination):
     if os.path.isfile(source):
-        shutil.move(source, destination)
+        os.rename(source, destination)
 
 def main():
     read_file(filepath)
-    sort()
-
+    sort_files()
+    
 main()
